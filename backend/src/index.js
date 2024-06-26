dfa5e6ef9de31ba56268a24692a839720e8b8d53
octylacetate <aoctyl@gmail.com>
2024-06-26 20:36:06 +0500

    new files k sth

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,15 +3,15 @@ import dotenv from "dotenv";
 import { connectDB } from "./db/index.js";
 
 dotenv.config({
-    path: './.env'
+    path: './backend\src\.env'
 })
 
 
-
+const port = 8000;
 connectDB()
 .then(() => {
-    app.listen(process.env.PORT || 8000, () => {
-        console.log(`⚙️ Server is running at port : ${process.env.PORT}`);
+    app.listen( port, () => { 
+        console.log(`⚙️ Server is running at port :${port}`);
     })
 })
 .catch((err) => {
