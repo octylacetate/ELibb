e23bcf97c3b0ac12a685d3a89df9c17667855e5c
M-Soban2002 <104456272+M-Soban2002@users.noreply.github.com>
2024-06-27 07:45:09 -0700

    fix .env bugs

--- backend/src/models/user.model.js
+++ backend/src/models/user.model.js
@@ -51,7 +51,7 @@ userSchema.methods.generateAccessToken = function(){
         {
             _id: this._id,
             email: this.email,
-            username: this.userName
+            username: this.username
         },
         process.env.ACCESS_TOKEN_SECRET,
         {
