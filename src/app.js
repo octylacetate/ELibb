a43b76b57e294697718e9ea1cedfba68d643a38a
M-Soban2002 <104456272+M-Soban2002@users.noreply.github.com>
2024-07-04 14:24:10 -0700

    fix multer bugs

---
+++ src/app.js
@@ -0,0 +1,30 @@
+import express from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+
+const app = express();
+
+app.use(cors({
+    origin: 'http://localhost:60741' ,   //frontend ki port dlni
+    credentials: true 
+}));
+app.use(express.json({limit:"16kb"}));
+app.use(express.urlencoded({
+    limit:"16kb",
+    extended: true
+}));
+app.use(express.static("public"));
+app.use(cookieParser());
+
+
+//routes import
+import userRouter from './routes/user.routes.js'
+
+
+//routes declaration
+app.use("/api/v1/users", userRouter)
+
+
+
+
+export {app} 
---
+++ src/db/index.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+
+
+const connectDB = async () => {
+    try {
+        const connectionInstance = await mongoose.connect("mongodb+srv://muhammadsoban490:VeF0voKbIPtUx0tT@cluster0.e82p2ep.mongodb.net/letsChat-db?retryWrites=true&w=majority&appName=Cluster0")
+        console.log(`\n MongoDB connected !! DB HOST: ${connectionInstance.connection.host}`);
+    } catch (error) {
+        console.log("MONGODB connection FAILED ", error);
+        process.exit(1)
+    }
+}
+
+export {connectDB}
